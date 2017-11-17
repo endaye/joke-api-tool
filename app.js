0734a5e9639d54395767bf4b6ec79ad0a467c6c8
Yuancheng Zhang <old.far.hot@gmail.com>
2017-11-17 02:23:42 -0600

    add plugin into package.json

--- app.js
+++ app.js
@@ -2,18 +2,19 @@
  * Created by En on 7/3/17.
  */
 
-var express = require('express');
-var app = express();
+var express = require("express"),
+    app = express(),
+    plugin = require("centaurs-test-plugin");
 
 var port = process.env.PORT || 3403;
-app.set('port', port);
+app.set("port", port);
 
-var jokes = require('./controllers/joke_controller.js')
+var jokes = require("./controllers/joke_controller.js");
 
-app.get('/', jokes.index);
+app.get("/", jokes.index);
 
-app.get('/api/joke/get', jokes.get_joke);
+app.get("/api/joke/get", jokes.get_joke);
 
-app.listen(app.get('port'), function () {
-    console.log('Server started at ' + port);
+app.listen(app.get("port"), function () {
+    console.log("Server started at " + port);
 });

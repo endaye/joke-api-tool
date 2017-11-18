4fcd9db73b9189bad3744d9e0bdc74e863ca1792
Yuancheng Zhang <old.far.hot@gmail.com>
2017-11-17 18:33:31 -0600

    put the process checking into plugin

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 
 var express = require('express');
 var app = express();
+var plugin = require('centaurs-test-plugin');
 
 var port = process.env.PORT || 3403;
 app.set('port', port);
@@ -14,6 +15,20 @@ app.get('/', jokes.index);
 
 app.get('/api/joke/get', jokes.get_joke);
 
+
+    plugin.catchErr();
+
 app.listen(app.get('port'), function () {
     console.log('Server started at ' + port);
+
+    plugin.setObj({
+        app_name: "joke-api"
+    });
+    plugin.showConfig();
+    plugin.sysCheck(10);
+
+setTimeout(()=>{
+    throw new Error('Whoops!');
+}, 5000);
+    
 });
